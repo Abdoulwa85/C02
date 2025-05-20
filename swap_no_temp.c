9f971d7b009d2c0cbc2b4acaca4a6eba31a3e199
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-20 21:36:19 +0100

    ajout du fichier swap

---
+++ swap_no_temp.c
@@ -0,0 +1,14 @@
+‎#include <stdio.h>
+‎// Ce programme permet de lire deux nombres et de les inverses sans utiliser de variables temporaires
+‎int main() {
+‎    int a, b;
+‎    // Lecture de deux nombres
+‎    scanf("%d %d", &a, &b);
+‎    // Échange sans variable temporaire
+‎    a = a + b;
+‎    b = a - b;
+‎    a = a - b;
+‎    printf("Après échange : a=%d, b=%d\n");
+    return 0;
+‎}
+‎
